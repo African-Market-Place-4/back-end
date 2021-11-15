fb2884c697ed07c3574060be6369aa7ae069d081
kennedybryant <kennedy_marisa96@yahoo.com>
2021-11-15 17:17:41 -0500

    Moving

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -16,21 +16,20 @@ router.post('/register', (req, res, next) => {
 })
 
 router.post('/login', (req, res, next) => {
-    // res.json('login route')
-    // const { username, password } = req.body
-    // Users.findBy({ username })
-    //     .then(([user]) => {
-    //         if (user && bcrypt.compareSync(password, req.user.password)) {
-    //             const token = tokenBuilder(user)
-    //             res.status(200).json({
-    //                 message: `Welcome, ${user.username}!`,
-    //                 token,
-    //             })
-    //         } else {
-    //             next({ status: 401, message: 'Invalid credentials'})
-    //         }
-    //     })
-    //     .catch(next)
+    const { username, password } = req.body
+    Users.findBy({ username })
+        .then(([user]) => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = tokenBuilder(user)
+                res.status(200).json({
+                    message: `Welcome, ${user.username}!`,
+                    token,
+                })
+            } else {
+                next({ status: 401, message: 'Invalid credentials'})
+            }
+        })
+        .catch(next)
 })
 
 module.exports = router
